7295ebba50082987c93f2cc2bd66150d9064a7b3
ANR14 <nariman.altinbekov@yandex.ru>
2020-03-04 20:06:50 +0600

    commit 1

---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React, {Fragment, useContext} from 'react'
+import {Search} from "../components/Search";
+import {Card} from "../components/Card";
+import {GithubContext} from "../context/github/githubContext";
+import {Loading} from "../components/Loading";
+
+export const Home = () => {
+
+  const {loading, users} = useContext(GithubContext)
+
+  return (
+    <Fragment>
+      <Search />
+
+      <div className="row">
+
+        {loading
+          ? <div className="col"><Loading /></div>
+          : users.map(user => (
+            <div className="col-sm-4 mb-4" key={user.id}>
+              <Card user={user} />
+            </div>
+          ))
+        }
+
+      </div>
+    </Fragment>
+  )
+}
